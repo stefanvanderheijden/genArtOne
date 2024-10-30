0b67dfac173576a21cf1f481f0734f0e002dbadb
Stefan van der Heijden <stefanvanderheijden@gmail.com>
2024-10-30 13:26:18 +0100

    Two dimensional forloop

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let margin = 50;
 
 let canvasContainer, containerWidth, containerHeight;
 
-let noOfLines = 30;
+let gridDimension = 30;
 
 function setup() {
   canvasContainer = select("#canvas-container");
@@ -24,23 +24,16 @@ function draw() {
   background("#ffedc0");
 
   let totalVerticalDistance = containerHeight - margin * 2;
-  let verticalStep = totalVerticalDistance / noOfLines;
+  let verticalStep = totalVerticalDistance / gridDimension;
 
   strokeWeight(1);
 
-  for (let index = 0; index < noOfLines + 1; index++) {
+  for (let index = 0; index < gridDimension + 1; index++) {
     let y = margin + index * verticalStep;
-    let xStart = margin;
-    let xEnd = containerWidth - margin;
 
-    line(xStart, y, xEnd, y);
-  }
-
-  for (let index = 0; index < noOfLines + 1; index++) {
-    let x = margin + index * verticalStep;
-    let yStart = margin;
-    let yEnd = containerWidth - margin;
-
-    line(x, yStart, x, yEnd);
+    for (let index = 0; index < gridDimension + 1; index++) {
+      let x = margin + index * verticalStep;
+      circle(x, y, 4);
+    }
   }
 }
